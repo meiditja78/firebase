61d15d71d9fb3073d4a3b433ed7c20f6ea66ae22
Meidi Tjahyady <meidy.tj@gmail.com>
2019-02-03 14:12:55 +0700

    Hello

--- src/App.js
+++ src/App.js
@@ -17,6 +17,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
+          Hello
             Learn React
           </a>
         </header>
